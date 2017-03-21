fb67f6493a2076790c44879fc048d67e0c669568
Daniel Laurindo <dslaurindo@gmail.com>
2017-03-21 12:34:10 -0300

    Finish started config webpack, react and jest

--- entry.js
+++
@@ -1,1 +0,0 @@
-document.write('Oi! How are you?');
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
 module.exports = {
-    entry: './entry.js',
-    output: {
-        path: __dirname,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            { test: '/\.css$/', loader: 'style!css' }
-        ]
-    }
+  entry: [
+    './app/entry.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'app/bundle.js'
+  },
+  module: {
+    loaders: [{
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './'
+  }
 };
